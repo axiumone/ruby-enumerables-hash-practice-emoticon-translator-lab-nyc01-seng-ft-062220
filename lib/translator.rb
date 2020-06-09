f86c41fb2c0195a1d4af47aa6027da16ecd3069d
Dmitry Sychev <axiumone@gmail.com>
2020-06-09 21:28:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,10 +16,10 @@ def load_library(file)
 end
 
 
-# def get_japanese_emoticon
-#   # code goes here
-# end
-#
-# def get_english_meaning
-#   # code goes here
-# end
+def get_japanese_emoticon
+  # code goes here
+end
+
+def get_english_meaning
+  # code goes here
+end
